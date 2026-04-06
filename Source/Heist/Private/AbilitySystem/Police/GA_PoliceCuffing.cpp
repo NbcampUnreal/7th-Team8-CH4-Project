cf32cb40f1ed0c5f603f482c947cf1f1351595b5
ha2m2 <hamin0691@gmail.com>
2026-04-06 16:46:35 +0900

    feat(cpp): 체포/이송 관련 GA, 컴포넌트, Core 추가

---
+++ Source/Heist/Private/AbilitySystem/Police/GA_PoliceCuffing.cpp
@@ -0,0 +1,85 @@
+﻿#include "AbilitySystem/Police/GA_PoliceCuffing.h"
+
+#include "Character/ThiefCharacter.h"
+#include "Character/HeistTags_State.h"
+#include "AbilitySystem/HeistTags_Event.h"
+#include "AbilitySystemComponent.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+UGA_PoliceCuffing::UGA_PoliceCuffing()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UGA_PoliceCuffing::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo)) return;
+
+	TargetThief = nullptr;
+
+	TArray<AActor*> OverlappingActors;
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
+
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(GetAvatarActorFromActorInfo());
+
+	//TODO(하민): 임시 타겟팅 (추후 Interaction 컴포넌트로 교체)
+	UKismetSystemLibrary::SphereOverlapActors(GetWorld(), GetAvatarActorFromActorInfo()->GetActorLocation(),
+		InteractRadius, ObjectTypes, AThiefCharacter::StaticClass(), ActorsToIgnore, OverlappingActors);
+
+	for (AActor* Actor : OverlappingActors)
+	{
+		AThiefCharacter* Thief = Cast<AThiefCharacter>(Actor);
+		if (!IsValid(Thief)) continue;
+
+		UAbilitySystemComponent* TargetASC = Thief->GetAbilitySystemComponent();
+		if (IsValid(TargetASC) && TargetASC->HasMatchingGameplayTag(HeistStateTags::State_Thief_Injured))
+		{
+			TargetThief = Thief;
+			break;
+		}
+	}
+
+	if (!IsValid(TargetThief))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	// [테스트 코드] 채널링 정상 작동 확인 후 복구
+	//StartChanneling(FName("Cuffing"));
+	OnChannelingCompleted();
+}
+
+void UGA_PoliceCuffing::OnChannelingCompleted()
+{
+	if (IsValid(TargetThief))
+	{
+		UAbilitySystemComponent* TargetASC = TargetThief->GetAbilitySystemComponent();
+		if (IsValid(TargetASC))
+		{
+			FGameplayEventData Payload;
+			Payload.Instigator = GetAvatarActorFromActorInfo();
+			Payload.Target = TargetThief;
+
+			TargetASC->HandleGameplayEvent(HeistEventTags::Event_CuffingComplete, &Payload);
+
+			// [테스트 코드] 도둑 파트가 없어서 경찰이 직접 태그 교체
+			TargetASC->RemoveLooseGameplayTag(HeistStateTags::State_Thief_Injured);
+			TargetASC->AddLooseGameplayTag(HeistStateTags::State_Thief_Cuffed);
+		}
+	}
+
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UGA_PoliceCuffing::OnChannelingCancelled()
+{
+	TargetThief = nullptr;
+}
---
+++ Source/Heist/Public/AbilitySystem/Police/GA_PoliceCuffing.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/HeistGameplayAbility.h"
+#include "GA_PoliceCuffing.generated.h"
+
+class AThiefCharacter;
+
+UCLASS()
+class HEIST_API UGA_PoliceCuffing : public UHeistGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UGA_PoliceCuffing();
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void OnChannelingCompleted() override;
+	virtual void OnChannelingCancelled() override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<AThiefCharacter> TargetThief;
+
+	// TODO(하민): Interaction 컴포넌트 완성 시 트리거 이벤트 데이터로 타겟을 넘겨받고 이 변수는 삭제 예정
+	UPROPERTY(EditDefaultsOnly, Category = "Heist|Cuffing")
+	float InteractRadius = 150.0f;
+};
