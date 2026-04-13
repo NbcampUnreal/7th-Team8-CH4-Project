efb00f635d0d20f8ffe46c6860c8056c9b83ea24
bowons <150906466+bowons@users.noreply.github.com>
2026-04-13 20:26:30 +0900

    feat: 체포 시 도둑 데롱데롱 레그돌 (#31)
    * asset(anim): 레그돌 에셋/Blendspace 수정/AO 사전작업 에셋/수갑 밧줄 에셋/ 추가
    
    * feat(bp): bp에 수갑 컴포넌트와 밧줄 컴포넌트 추가/테스트맵
    
    * chore(ini): CableComopnent 사용으로 인한 종속성 추가
    
    * feat(cpp): Escort 레그돌 처리 완료

--- Source/Heist/Private/Character/PoliceCharacter.cpp
+++ Source/Heist/Private/Character/PoliceCharacter.cpp
@@ -1,9 +1,11 @@
 #include "Character/PoliceCharacter.h"
 
+#include "Components/ThiefEscortComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 APoliceCharacter::APoliceCharacter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	GetCharacterMovement()->RotationRate = FRotator(0.f, MaxRotationRateYaw, 0.f);
+	EscortComponent = CreateDefaultSubobject<UThiefEscortComponent>(TEXT("EscortComponent"));
 }
