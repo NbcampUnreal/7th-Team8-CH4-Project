f86d00134f4abf6d52d539037fd51c3e652cd5e7
ha2m2 <hamin0691@gmail.com>
2026-04-14 17:01:15 +0900

    feat(cpp): 발자국 AnimNotify 생성

---
+++ Source/Heist/Public/Animation/AnimNotify_HeistFootstep.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_HeistFootstep.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class HEIST_API UAnimNotify_HeistFootstep : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
