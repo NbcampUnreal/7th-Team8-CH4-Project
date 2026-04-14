2ddcb3095157db04f8b754b942be2851050f6f85
ha2m2 <hamin0691@gmail.com>
2026-04-14 17:04:07 +0900

    feat(cpp): 수갑 해제 소음 발생

--- Source/Heist/Private/AbilitySystem/Thief/GA_Thief_HelpCuffed.cpp
+++ Source/Heist/Private/AbilitySystem/Thief/GA_Thief_HelpCuffed.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "AbilitySystem/Thief/GA_Thief_HelpCuffed.h"
@@ -7,16 +7,18 @@
 #include "AbilitySystem/HeistTags_Ability.h"
 #include "Character/HeistTags_State.h"
 #include "Character/ThiefCharacter.h"
+#include "Components/HeistNoiseComponent.h"
+#include "Data/HeistSoundData.h"
 
 UGA_Thief_HelpCuffed::UGA_Thief_HelpCuffed()
 {
 	ActivationPolicy = EHeistAbilityActivationPolicy::OnGameplayEvent;
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
 	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
-	
+
 	AbilityTags.AddTag(HeistAbilityTags::Ability_Thief_HelpCuffed);
 	//CancelAbilitiesWithTag.AddTag(HeistStateTags::State_Stunned);
-	
+
 	// Trigger Data를 세팅해주는 것 만으로 이 어빌리티 시스템은 GAS에 등록된 이상
 	// GameplayEvent에 의해 Actiavated 될 수 있습니다. 와우
 	FAbilityTriggerData TriggerData;
@@ -30,13 +32,13 @@ void UGA_Thief_HelpCuffed::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 	const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-	
+
 	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
 	{
 		CancelAbility(Handle, ActorInfo, ActivationInfo, true);
 		return;
 	}
-	
+
 	// 트리거 데이터로 인해 켜졌을 경우? -> 트리거 데이터에서 타겟 액터를 찾아서 그 ASC를 캐싱해둬야겠죠
 	const AActor* TargetActor = TriggerEventData ? TriggerEventData->Target.Get() : nullptr;
 	const AThiefCharacter* Thief = Cast<AThiefCharacter>(TargetActor);
@@ -57,12 +59,38 @@ void UGA_Thief_HelpCuffed::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 	// FChannelingData의 RowName과 맞출 것
 	// StartChanneling이 GE_Channeling을 적용하여 State.ActionDisabled / State.Channeling 부여
 	StartChanneling(FName("HelpCuffed"));
+
+	if (HasAuthority(&CurrentActivationInfo))
+	{
+		AThiefCharacter* TargetThief = Cast<AThiefCharacter>(const_cast<AActor*>(TargetActor));
+		if (IsValid(TargetThief))
+		{
+			UHeistNoiseComponent* NoiseComponent = TargetThief->GetHeistNoiseComponent();
+			if (IsValid(NoiseComponent))
+			{
+				NoiseComponent->StartChannelingNoise(EHeistSoundType::Cuffing);
+			}
+		}
+	}
 }
 
 void UGA_Thief_HelpCuffed::EndAbility(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
+	if (HasAuthority(&CurrentActivationInfo) && TargetASC.IsValid())
+	{
+		AThiefCharacter* TargetThief = Cast<AThiefCharacter>(TargetASC->GetAvatarActor());
+		if (IsValid(TargetThief))
+		{
+			UHeistNoiseComponent* NoiseComponent = TargetThief->GetHeistNoiseComponent();
+			if (IsValid(NoiseComponent))
+			{
+				NoiseComponent->StopChannelingNoise();
+			}
+		}
+	}
+
 	TargetASC = nullptr;
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
