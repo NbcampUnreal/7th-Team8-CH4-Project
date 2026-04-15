cc1842509eaad3cf5658d214027696bd6f40c367
ha2m2 <hamin0691@gmail.com>
2026-04-15 14:08:24 +0900

    feat(cpp): 탐지 알림 및 태그 추가

--- Source/Heist/Private/Systems/Messaging/HeistTags_Message.cpp
+++ Source/Heist/Private/Systems/Messaging/HeistTags_Message.cpp
@@ -1,23 +1,25 @@
-#include "Systems/Messaging/HeistTags_Message.h"
+﻿#include "Systems/Messaging/HeistTags_Message.h"
 
 namespace HeistMessageTags
 {
-    UE_DEFINE_GAMEPLAY_TAG(Message_SystemMenu_Toggle, "Message.SystemMenu.Toggle");
+	UE_DEFINE_GAMEPLAY_TAG(Message_SystemMenu_Toggle, "Message.SystemMenu.Toggle");
 
-    UE_DEFINE_GAMEPLAY_TAG(Message_Lobby_PlayersChanged, "Message.Lobby.PlayersChanged");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Lobby_ReadyStateChanged, "Message.Lobby.ReadyStateChanged");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Lobby_InviteCodeChanged, "Message.Lobby.InviteCodeChanged");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Lobby_PlayersChanged, "Message.Lobby.PlayersChanged");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Lobby_ReadyStateChanged, "Message.Lobby.ReadyStateChanged");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Lobby_InviteCodeChanged, "Message.Lobby.InviteCodeChanged");
 
-    UE_DEFINE_GAMEPLAY_TAG(Message_Ping, "Message.Ping");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Ping_Danger, "Message.Ping.Danger");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Ping_Move, "Message.Ping.Move");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Ping_Item, "Message.Ping.Item");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Ping_Rescue, "Message.Ping.Rescue");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Ping, "Message.Ping");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Ping_Danger, "Message.Ping.Danger");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Ping_Move, "Message.Ping.Move");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Ping_Item, "Message.Ping.Item");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Ping_Rescue, "Message.Ping.Rescue");
 
-    UE_DEFINE_GAMEPLAY_TAG(Message_Alarm, "Message.Alarm");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Alarm_Theft, "Message.Alarm.Theft");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Alarm_GPS, "Message.Alarm.GPS");
-    UE_DEFINE_GAMEPLAY_TAG(Message_Alarm_Global, "Message.Alarm.Global");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Alarm, "Message.Alarm");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Alarm_Theft, "Message.Alarm.Theft");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Alarm_GPS, "Message.Alarm.GPS");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Alarm_Global, "Message.Alarm.Global");
 
-    UE_DEFINE_GAMEPLAY_TAG(Message_Voice_TalkingStateChanged, "Message.Voice.TalkingStateChanged");
+	UE_DEFINE_GAMEPLAY_TAG(Message_UI_SoundDetected, "Message.UI.SoundDetected");
+	UE_DEFINE_GAMEPLAY_TAG(Message_UI_FlashlightAlert, "Message.UI.FlashlightAlert");
+	UE_DEFINE_GAMEPLAY_TAG(Message_Voice_TalkingStateChanged, "Message.Voice.TalkingStateChanged");
 }
