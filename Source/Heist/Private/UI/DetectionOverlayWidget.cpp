aabdf19963c4d057fa2567b5b28c2588b30b7e7e
ha2m2 <hamin0691@gmail.com>
2026-04-15 14:07:40 +0900

    feat(cpp): 탐지 오버레이 생성

---
+++ Source/Heist/Private/UI/DetectionOverlayWidget.cpp
@@ -0,0 +1,45 @@
+﻿#include "UI/DetectionOverlayWidget.h"
+
+#include "Systems/Messaging/HeistTags_Message.h"
+#include "Systems/Messaging/HeistMessageTypes.h"
+
+void UDetectionOverlayWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	UHeistMessageSubsystem& MessageSubsystem = UHeistMessageSubsystem::Get(GetWorld());
+
+	SoundDetectedHandle = MessageSubsystem.RegisterListener<FHeistSoundDetectedMessage>(
+		HeistMessageTags::Message_UI_SoundDetected,
+		[this](FGameplayTag Channel, const FHeistSoundDetectedMessage& Message)
+		{
+			HandleSoundDetectedMessage(Channel, Message);
+		}
+	);
+
+	FlashlightAlertHandle = MessageSubsystem.RegisterListener<FHeistFlashlightAlertMessage>(
+		HeistMessageTags::Message_UI_FlashlightAlert,
+		[this](FGameplayTag Channel, const FHeistFlashlightAlertMessage& Message)
+		{
+			HandleFlashlightAlertMessage(Channel, Message);
+		}
+	);
+}
+
+void UDetectionOverlayWidget::NativeDestruct()
+{
+	SoundDetectedHandle.Unregister();
+	FlashlightAlertHandle.Unregister();
+
+	Super::NativeDestruct();
+}
+
+void UDetectionOverlayWidget::HandleSoundDetectedMessage(FGameplayTag Channel, const FHeistSoundDetectedMessage& Message)
+{
+	OnSoundDetected(Message.OriginLocation, Message.DetectionRadius);
+}
+
+void UDetectionOverlayWidget::HandleFlashlightAlertMessage(FGameplayTag Channel, const FHeistFlashlightAlertMessage& Message)
+{
+	OnFlashlightAlertStateChanged(Message.bIsDetected);
+}
