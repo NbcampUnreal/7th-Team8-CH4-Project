452336e0d5a42f55176731e82bfecc5bf4579a14
dooyeon kang <dyhouse0517@gmail.com>
2026-04-15 18:03:16 +0900

    chore: 탭 들여쓰기 정규화

--- Source/Heist/Public/Systems/Messaging/HeistMessageTypes.h
+++ Source/Heist/Public/Systems/Messaging/HeistMessageTypes.h
@@ -10,44 +10,44 @@
 USTRUCT()
 struct FHeistSystemMenuToggleMessage
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 };
 
 USTRUCT()
 struct FHeistLobbyPlayersChangedMessage
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
-    UPROPERTY()
-    TArray<FString> PlayerNames;
+	UPROPERTY()
+	TArray<FString> PlayerNames;
 };
 
 USTRUCT()
 struct FHeistLobbyInviteCodeChangedMessage
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
-    UPROPERTY()
-    FString InviteCode;
+	UPROPERTY()
+	FString InviteCode;
 };
 
 USTRUCT()
 struct FHeistLobbyReadyStateChangedMessage
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 };
 
 USTRUCT()
 struct FHeistVoiceTalkingStateMessage
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
-    // 발화 상태가 바뀐 플레이어
-    UPROPERTY()
-    TObjectPtr<APlayerState> PlayerState = nullptr;
+	// 발화 상태가 바뀐 플레이어
+	UPROPERTY()
+	TObjectPtr<APlayerState> PlayerState = nullptr;
 
-    UPROPERTY()
-    bool bIsTalking = false;
+	UPROPERTY()
+	bool bIsTalking = false;
 };
 
 USTRUCT()
