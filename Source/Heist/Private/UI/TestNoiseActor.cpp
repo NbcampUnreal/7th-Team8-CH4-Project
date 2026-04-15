1abe8041c682789194edbfef0214328d8f2fa05a
ha2m2 <hamin0691@gmail.com>
2026-04-15 11:18:05 +0900

    chore(cpp): 테스트용 소음 발생 Actor 생성

---
+++ Source/Heist/Private/UI/TestNoiseActor.cpp
@@ -0,0 +1,35 @@
+﻿#include "UI/TestNoiseActor.h"
+
+#include "Systems/Messaging/HeistMessageTypes.h"
+#include "Systems/Messaging/HeistMessageSubsystem.h"
+#include "Systems/Messaging/HeistTags_Message.h"
+
+#include "TimerManager.h"
+#include "Components/SphereComponent.h"
+
+ATestNoiseActor::ATestNoiseActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	USphereComponent* SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->InitSphereRadius(50.0f);
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = SphereComp;
+}
+
+void ATestNoiseActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorldTimerManager().SetTimer(TestTimerHandle, this, &ThisClass::FireTestNoise, 3.0f, true);
+}
+
+void ATestNoiseActor::FireTestNoise()
+{
+	FHeistSoundDetectedMessage Message;
+	Message.OriginLocation = GetActorLocation();
+	Message.DetectionRadius = 1000.0f;
+
+	UHeistMessageSubsystem& MessageSubsystem = UHeistMessageSubsystem::Get(this);
+	MessageSubsystem.BroadcastMessage(HeistMessageTags::Message_UI_SoundDetected, Message);
+}
