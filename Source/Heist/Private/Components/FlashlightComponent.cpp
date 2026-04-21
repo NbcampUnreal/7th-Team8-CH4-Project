b2676ed164d3a1d8d5f46384012bca3e414f905c
ha2m2 <hamin0691@gmail.com>
2026-04-22 03:31:15 +0900

    refactor: 손전등 판정 로직 중복 제거 및 Outdoor 태그 체크 방식 수정

--- Source/Heist/Private/Components/FlashlightComponent.cpp
+++ Source/Heist/Private/Components/FlashlightComponent.cpp
@@ -4,9 +4,6 @@
 #include "Character/ThiefCharacter.h"
 #include "Character/HeistTags_State.h"
 #include "Components/HeistTransparencyComponent.h"
-#include "Systems/Messaging/HeistMessageTypes.h"
-#include "Systems/Messaging/HeistMessageSubsystem.h"
-#include "Systems/Messaging/HeistTags_Message.h"
 
 #include "AbilitySystemComponent.h"
 #include "GameFramework/Pawn.h"
@@ -43,95 +40,58 @@ void UFlashlightComponent::StopLocalVision()
 	}
 }
 
+bool UFlashlightComponent::IsTargetInFlashlightCone(AActor* TargetActor, const FVector& PoliceLocation, float EffectiveHalfAngle) const
+{
+	if (!IsValid(TargetActor)) return false;
+
+	AActor* OwnerActor = GetOwner();
+	if (!IsValid(OwnerActor)) return false;
+
+	const FVector TargetLocation = TargetActor->GetActorLocation();
+	const FVector PoliceLocation2D(PoliceLocation.X, PoliceLocation.Y, 0.f);
+	const FVector TargetLocation2D(TargetLocation.X, TargetLocation.Y, 0.f);
+
+	// 1. 거리 체크
+	if (FVector::Distance(PoliceLocation2D, TargetLocation2D) > FlashlightRadius) return false;
+
+	// 2. 각도 체크
+	const FVector DirectionToTarget = (TargetLocation2D - PoliceLocation2D).GetSafeNormal();
+	FVector PoliceForward = OwnerActor->GetActorForwardVector();
+	PoliceForward.Z = 0.f;
+	PoliceForward.Normalize();
+
+	const float CosineThreshold = FMath::Cos(FMath::DegreesToRadians(EffectiveHalfAngle));
+	if (FVector::DotProduct(PoliceForward, DirectionToTarget) < CosineThreshold) return false;
+
+	// 3. LineTrace 체크
+	FHitResult HitResult;
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(OwnerActor);
+
+	const bool bHitSomething = GetWorld()->LineTraceSingleByChannel(HitResult, PoliceLocation, TargetLocation, ECC_Visibility, QueryParams);
+
+	if (!bHitSomething) return true;
+
+	AActor* HitActor = HitResult.GetActor();
+	return HitActor == TargetActor || (IsValid(HitActor) && HitActor->IsA<AThiefCharacter>());
+}
+
 bool UFlashlightComponent::IsThiefInFlashlight(AThiefCharacter* Thief, bool bWasPreviouslyVisible) const
 {
 	UAbilitySystemComponent* ThiefAbilitySystemComponent = Thief->GetAbilitySystemComponent();
 	if (!IsValid(ThiefAbilitySystemComponent)) return false;
 
-	// 야외에 있는 도둑은 항상 보임
-	if (ThiefAbilitySystemComponent->HasMatchingGameplayTag(HeistStateTags::Zone_Outdoor)) return true;
+	const float EffectiveHalfAngle = bWasPreviouslyVisible ? (FlashlightHalfAngle + FlashlightHysteresisAngle) : FlashlightHalfAngle;
 
-	AActor* OwnerActor = GetOwner();
-	if (!IsValid(OwnerActor)) return false;
-
-	FVector PoliceLocation = OwnerActor->GetActorLocation();
-	FVector ThiefLocation = Thief->GetActorLocation();
-
-	FVector PoliceLocation2D = FVector(PoliceLocation.X, PoliceLocation.Y, 0.f);
-	FVector ThiefLocation2D = FVector(ThiefLocation.X, ThiefLocation.Y, 0.f);
-
-	if (FVector::Distance(PoliceLocation2D, ThiefLocation2D) > FlashlightRadius) return false;
-
-	FVector DirectionToThief = (ThiefLocation2D - PoliceLocation2D).GetSafeNormal();
-	FVector PoliceForward = OwnerActor->GetActorForwardVector();
-	PoliceForward.Z = 0.f;
-	PoliceForward.Normalize();
-
-	const float EffectiveHalfAngle = bWasPreviouslyVisible
-		? (FlashlightHalfAngle + FlashlightHysteresisAngle)
-		: FlashlightHalfAngle;
-	const float CosineThreshold = FMath::Cos(FMath::DegreesToRadians(EffectiveHalfAngle));
-
-	if (FVector::DotProduct(PoliceForward, DirectionToThief) < CosineThreshold)	return false;
-
-	FHitResult HitResult;
-	FCollisionQueryParams QueryParams;
-	QueryParams.AddIgnoredActor(OwnerActor);
-
-	const bool bHitSomething = GetWorld()->LineTraceSingleByChannel(
-		HitResult,
-		PoliceLocation,
-		ThiefLocation,
-		ECC_Visibility,
-		QueryParams
-	);
-
-	if (!bHitSomething) return true;
-	return HitResult.GetActor() == Thief;
+	return IsTargetInFlashlightCone(Thief, GetOwner()->GetActorLocation(), EffectiveHalfAngle);
 }
 
 bool UFlashlightComponent::IsItemInFlashlight(AItemActor* Item, const FVector& PoliceLocation) const
 {
-	if (!IsValid(Item)) return false;
-
-	const FVector ItemLocation = Item->GetActorLocation();
-
-	const FVector PoliceLocation2D = FVector(PoliceLocation.X, PoliceLocation.Y, 0.f);
-	const FVector ItemLocation2D = FVector(ItemLocation.X, ItemLocation.Y, 0.f);
-
-	if (FVector::Distance(PoliceLocation2D, ItemLocation2D) > FlashlightRadius) return false;
-
-	const FVector DirectionToItem = (ItemLocation2D - PoliceLocation2D).GetSafeNormal();
-
-	AActor* OwnerActor = GetOwner();
-	if (!IsValid(OwnerActor)) return false;
-
-	FVector PoliceForward = OwnerActor->GetActorForwardVector();
-	PoliceForward.Z = 0.f;
-	PoliceForward.Normalize();
-
-	const float CosineThreshold = FMath::Cos(FMath::DegreesToRadians(FlashlightHalfAngle));
-	if (FVector::DotProduct(PoliceForward, DirectionToItem) < CosineThreshold) return false;
-
-	FHitResult HitResult;
-	FCollisionQueryParams QueryParams;
-	QueryParams.AddIgnoredActor(OwnerActor);
-
-	const bool bHitSomething = GetWorld()->LineTraceSingleByChannel(
-		HitResult,
-		PoliceLocation,
-		ItemLocation,
-		ECC_Visibility,
-		QueryParams
-	);
-
-	if (!bHitSomething) return true;
-
-	AActor* HitActor = HitResult.GetActor();
-
-	return HitActor == Item || (IsValid(HitActor) && HitActor->IsA<AThiefCharacter>());
+	return IsTargetInFlashlightCone(Item, PoliceLocation, FlashlightHalfAngle);
 }
 
+// TODO(하민): 추후 스폰된 도둑과 아이템 목록을 캐싱(Caching)해두고 그 배열을 순회하는 방식으로 최적화
 void UFlashlightComponent::ProcessLocalVision()
 {
 	AActor* OwnerActor = GetOwner();
@@ -159,8 +119,9 @@ void UFlashlightComponent::ProcessLocalVision()
 		const float DistanceSquared = FVector::DistSquaredXY(PoliceLocation, Thief->GetActorLocation());
 		const bool bInCloseVision = (DistanceSquared <= FMath::Square(CloseVisionRadius));
 
+		const bool bIsOutdoor = ThiefAbilitySystemComponent->HasMatchingGameplayTag(HeistStateTags::Zone_Outdoor);
 		const bool bWasVisible = PreviouslyVisibleThieves.Contains(Thief);
-		const bool bIsNowVisible = bIsInCone || bInCloseVision;
+		const bool bIsNowVisible = bIsInCone || bInCloseVision || bIsOutdoor;
 
 		if (bIsNowVisible != bWasVisible)
 		{
