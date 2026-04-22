e8f936cc5028d42b14b0e3bb5285e817195d5c01
ha2m2 <hamin0691@gmail.com>
2026-04-22 23:52:54 +0900

    chore(cpp): 탐지 기능 분리 및 사운드 재생 로직 제거

--- Source/Heist/Public/Components/HeistNoiseComponent.h
+++ Source/Heist/Public/Components/HeistNoiseComponent.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
@@ -6,7 +6,6 @@
 
 class UDataTable;
 class UAbilitySystemComponent;
-class UAudioComponent;
 struct FHeistSoundData;
 
 enum class EHeistSoundType : uint8;
@@ -29,34 +28,24 @@ public:
 	void StopChannelingNoise();
 
 protected:
-	UPROPERTY(EditDefaultsOnly, Category = "Heist|Audio")
-	TObjectPtr<UDataTable> SoundDataTable;
-
-	UPROPERTY()
-	TObjectPtr<UAudioComponent> ActiveChannelingAudio;
-
 	UFUNCTION(Server, Reliable)
 	void Server_StartChannelingNoise(EHeistSoundType SoundType);
-
-	UFUNCTION(NetMulticast, Reliable)
-	void Multicast_StartChannelingNoise(EHeistSoundType SoundType);
 
 	UFUNCTION(Server, Reliable)
 	void Server_StopChannelingNoise();
 
-	UFUNCTION(NetMulticast, Reliable)
-	void Multicast_StopChannelingNoise();
-
-	// 1. 서버 권한으로 실제 로직을 처리하는 함수
 	UFUNCTION(Server, Reliable)
 	void Server_ProcessNoise(EHeistSoundType SoundType, FVector OriginLocation);
 
-	// 2. 모든 클라이언트에게 오디오 재생을 지시하는 함수 (엔진 자체 감쇠 적용)
-	UFUNCTION(NetMulticast, Unreliable)
-	void Multicast_PlaySound(EHeistSoundType SoundType, FVector OriginLocation);
-
-	// 내부 헬퍼 함수들
 	const FHeistSoundData* GetSoundData(EHeistSoundType SoundType) const;
 	float CalculateFinalDetectionRadius(const FHeistSoundData* SoundData, UAbilitySystemComponent* OwnerASC) const;
 	void DetectPoliceAndSendEvent(const FHeistSoundData* SoundData, float FinalRadius, FVector OriginLocation);
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Heist|Audio")
+	TObjectPtr<UDataTable> SoundDataTable;
+
+	float LastFootstepTime = 0.0f;
+
+	static constexpr float FootstepDebounceTime = 0.15f;
 };
