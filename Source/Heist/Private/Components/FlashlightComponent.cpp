145752936445035ecce92327c7c093e9c451c429
ha2m2 <hamin0691@gmail.com>
2026-04-22 11:48:27 +0900

    chore(cpp): RestoreAllThiefVisibility 구현부 복구 (#75)

--- Source/Heist/Private/Components/FlashlightComponent.cpp
+++ Source/Heist/Private/Components/FlashlightComponent.cpp
@@ -40,6 +40,31 @@ void UFlashlightComponent::StopLocalVision()
 	}
 }
 
+void UFlashlightComponent::RestoreAllThiefVisibility()
+{
+	UWorld* World = GetWorld();
+	if (!IsValid(World)) return;
+
+	for (TActorIterator<AThiefCharacter> It(World); It; ++It)
+	{
+		AThiefCharacter* Thief = *It;
+		if (!IsValid(Thief)) continue;
+
+		UHeistTransparencyComponent* TransparencyComponent = Thief->GetComponentByClass<UHeistTransparencyComponent>();
+		if (IsValid(TransparencyComponent))
+		{
+			TransparencyComponent->SetTargetVisibility(true);
+		}
+		else if (USkeletalMeshComponent* ThiefMesh = Thief->GetMesh())
+		{
+			ThiefMesh->SetVisibility(true, true);
+		}
+	}
+
+	PreviouslyVisibleThieves.Reset();
+	PreviouslyThievesInCone.Reset();
+}
+
 bool UFlashlightComponent::IsTargetInFlashlightCone(AActor* TargetActor, const FVector& PoliceLocation, float EffectiveHalfAngle) const
 {
 	if (!IsValid(TargetActor)) return false;
