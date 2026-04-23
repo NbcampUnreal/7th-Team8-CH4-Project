3b1e133fb03f8dd88c764df07d00b1aa4684aa71
brian521 <59503208+brian521@users.noreply.github.com>
2026-04-23 16:29:32 +0900

    chore(cpp): 탈출시스템 로그 추가 (#88)

--- Source/Heist/Private/Components/HeistGameOverPhaseComponent.cpp
+++ Source/Heist/Private/Components/HeistGameOverPhaseComponent.cpp
@@ -19,6 +19,10 @@ void UHeistGameOverPhaseComponent::StartEngineChanneling()
 			EngineChannelingDuration,
 			false);
 	}
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT("엔진이 작동을 시작했습니다!"));
+	}
 	UE_LOG(LogTemp, Warning, TEXT("엔진 채널링 시작"));
 }
 
@@ -31,5 +35,10 @@ void UHeistGameOverPhaseComponent::EngineChannelingEnd()
 	if (!HeistGS) return;
 
 	HeistGS->SetEngineChannelingEnd(true);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT("출발 준비 완료! 문을 닫고 출발하세요!"));
+	}
 	UE_LOG(LogTemp, Warning, TEXT("엔진 채널링 종료"));
 }
